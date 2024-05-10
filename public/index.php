510e3ade3117973998b19f7d355baa7a822e08c4
Paul M. Jones <pmjones@pmjones.io>
2024-05-10 14:38:30 -0500

    apply styling

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 use Qiq\Template;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
 $template = Template::new(dirname(__DIR__) . '/resources', extension: '.qiq.php');
 $template->setView('index');
 echo $template();
--- public/styler.php
+++ public/styler.php
@@ -7,12 +7,16 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 $request = new Request();
-$service = new Service(new Styler(
-    eol: "\n",
-    lineLen: (int) ($request->input['line_len'] ?? 88),
-    indentLen: (int) ($request->input['indent_len'] ?? 4),
-    indentTab: (bool) ($request->input['indent_tab'] ?? false),
-));
+
+$service = new Service(
+    new Styler(
+        eol: "\n",
+        lineLen: (int) ($request->input['line_len'] ?? 88),
+        indentLen: (int) ($request->input['indent_len'] ?? 4),
+        indentTab: (bool) ($request->input['indent_tab'] ?? false),
+    ),
+);
+
 $code = $request->input['original_code'] ?? '';
 $code = $service($code);
 $highlighter = new \Highlight\Highlighter();
